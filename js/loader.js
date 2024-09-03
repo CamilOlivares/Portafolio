a82cfd3885ed0a4778f1561419c0d73526f47237
CamilOlivares <camilo.olivares.1984@gmail.com>
2024-09-03 17:18:45 -0400

    Se redujo a 1 segundo el loading

--- js/loader.js
+++ js/loader.js
@@ -1,9 +1,9 @@
 // loader.js
 
 window.addEventListener("load", function() {
-  // Mantener el loader visible durante 2 segundos
+  // Mantener el loader visible durante 1 segundos
   setTimeout(function() {
     document.getElementById("loader").style.display = "none"; // Oculta el loader
     document.getElementById("main-content").style.display = "block"; // Muestra el contenido principal
-  }, 2000); // 2000 milisegundos = 2 segundos
+  }, 1000); // 1000 milisegundos = 1 segundos
 });
